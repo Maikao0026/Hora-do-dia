c4de57f4fc3db4332c42d91bd1fc038bf99ce735
Michael Douglas <maikao0026@gmail.com>
2023-03-02 23:14:49 -0300

    atualizando nom dos arquivos

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function carregar(){
   if (horas>4 && horas<12) {
     //BOM DIA!
     document.getElementsByTagName('h1')[0].innerText ='BOM DIA!';
-    imagem.src = "./pexels-aliaksei-semirski-14458378.jpg";
+    imagem.src = "./sources/pexels-aliaksei-semirski-14458378.jpg";
     window.document.body.style.background = "#DEC6AE";
   }else if (horas<=4 || horas>=18){
     //BOA NOITE!
     document.getElementsByTagName('h1')[0].innerText ='BOA NOITE!'
-    imagem.src="./pexels-eberhard-grossgasteiger-2098427.jpg"
+    imagem.src = "./sources/pexels-eberhard-grossgasteiger-2098427.jpg";
     window.document.body.style.background = "#222E3C";
   }else{
     // BOA TARDE!
-    imagem.src="./pexels-thuong-d-12810433.jpg"
+    imagem.src = "./sources/pexels-thuong-d-12810433.jpg";
     window.document.body.style.background = "#EDA954";
     document.getElementsByTagName("h1")[0].innerText = "BOA TARDE!";
   }
